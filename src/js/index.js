bd80f0b741290c545283080cf1b1b5080726c89d
Mappel21 <Mappel7676@gmail.com>
2021-07-31 23:32:50 +0000

    adding firebase command to index

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,19 @@ import "../styles/index.scss";
 //import your own components
 import Layout from "./layout";
 
+// firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDMm4V-ZsbuSaw0tlem_JdUAVb4KJ60d0M",
+    authDomain: "auth-test-5e4b7.firebaseapp.com",
+    projectId: "auth-test-5e4b7",
+    storageBucket: "auth-test-5e4b7.appspot.com",
+    messagingSenderId: "669842658580",
+    appId: "1:669842658580:web:5c3138abda4404cf22de01",
+    measurementId: "G-7E7CXHDR2T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
